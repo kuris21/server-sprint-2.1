e85083ef4968db422b6a7dbf6e17a9c52f02d7c1
Kurisutopha <christopher_t_williams@brown.edu>
2024-09-25 22:59:33 -0400

    refactor deleted duplicate

--- server-kuris21-pandapandabra-main/CSCI0320-CSV1-main/csv/src/main/java/ObjectCreators/CreatorFromRow.java
+++
@@ -1,9 +0,0 @@
-package ObjectCreators;
-
-import CSV_Exceptions.FactoryFailureException;
-import java.util.List;
-
-public interface CreatorFromRow <T> {
-  T create(List<String> row) throws FactoryFailureException;
-}
-
